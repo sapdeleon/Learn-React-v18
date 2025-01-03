7b52762fee1b7113018967ee1b874c291656841e
Samuel de Leon <sapdeleon@msn.com>
2025-01-03 15:41:48 +0800

    Learn variable import/export

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import {add, multiply, subtract, divide, triplePi} from "../math.jsx";
 
 function List() {
     return (
         <ul>
-            <li>Bacon</li>
-            <li>Ham</li>
-            <li>Eggs</li>
+            <li>Add - {add(3, 4)}</li>
+            <li>Multiply - {multiply(4, 7)}</li>
+            <li>Subtract - {subtract(89, 25)}</li>
+            <li>Divice - {divide(45, 9)}</li>
+            <li>Triple PI - {triplePi()}</li>
         </ul>
     );
 };
---
+++ src/math.jsx
@@ -0,0 +1,24 @@
+const pi = 3.14159265;
+
+function add(n1, n2) {
+    return (n1 + n2);
+}
+
+function multiply(n1, n2) {
+    return (n1 * n2);
+}
+
+function subtract(n1, n2) {
+    return (n1 - n2);
+}
+
+function divide(n1, n2) {
+    return (n1 / n2);
+}
+
+function triplePi() {
+    return (3 * pi);
+}
+
+export default pi;
+export {add, multiply, subtract, divide, triplePi};
